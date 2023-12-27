69ae39f9fe4cf821bb1ec6cf0083b23ba65446a5
ho3khaleghi <hossein.khaleghifard@gmail.com>
2023-12-27 14:38:03 +0100

    Made the board of user and computer

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("                 ",chr(7140) ,"Battleship Game", chr(7140))
+print("       Your board                     Computer board  ")
+print(chr(4502) * 56)
+print("   A B C D E F G H I J    ",chr(8214),"      A B C D E F G H I J")
+for x in range(10):
+    row_number = ""
+    if x < 9:
+        row_number += "0"
+    
+    row_number += str(x + 1)
+    print(row_number, "_ " * 10, "  ",chr(8214),"  ", row_number, "_ " * 10)
+    
