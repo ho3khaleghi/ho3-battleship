dd99c4273bd6cb3f92aa474d5982abc08f58240d
ho3khaleghi <hossein.khaleghifard@gmail.com>
2023-12-28 23:29:09 +0100

    Defined some functions that will generate random spots for the computer ship and random spot for the computer hit, Added some comments to the code to understand the code better

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Board_cell:
     def __init__(self, is_ship, is_hit):
@@ -8,8 +8,11 @@ class Board_cell:
 computer_board = [Board_cell(False, False) for _ in range(100)]
 user_board = [Board_cell(False, False) for _ in range(100)]
 user_score = 0
+computer_score = 0
 
 def main():
+    # The main logic of the game which can control the flow of the code aswell
+
     draw_boards()
     for _ in range(5):
         define_ship()
@@ -21,10 +24,11 @@ def main():
             cordinate = position_translator(position)
             hit(cordinate)
             draw_boards()
-            game_over = user_score == 2
+            game_over = user_score == 15 or computer_score == 15
 
 
 def draw_ship(begin: int, direction):
+    # Set the location of the user ship by the location and direction that user entered
     
     user_board[begin].is_ship = True
     if direction == "V":
@@ -37,6 +41,8 @@ def draw_ship(begin: int, direction):
 
 
 def validate_direction(direction) -> bool:
+    # Validate that the entry of the user should be only vertically or horizontally
+
     if direction != "V" and direction != "H":   
         print("Please enter either V or H.")
         return False
@@ -61,11 +67,20 @@ def define_ship():
         define_ship()
 
 
-    
+def computer_ships():
+    # Generate random position for computer ships
+    # random.randint(1, 100)
+    pass
         
-        
+
+def computer_hit():
+    # Generate random shot for computer
+
+    pass
             
 def ship_validator(position: int, direction) -> bool:
+    # Validate that user can put the ship in the entry position or not
+
     if direction == "V":
         y = 10
     else:
@@ -77,6 +92,8 @@ def ship_validator(position: int, direction) -> bool:
     return True
 
 def draw_boards():
+    # Draw the user board and computer board
+
     print("                 ",chr(7140) ,"Battleship Game", chr(7140))
     print("       Your board                     Computer board  ")
     print(chr(4502) * 56)
@@ -112,6 +129,8 @@ def draw_boards():
 
 
 def validate_cordinate(position: str) -> bool:
+    # Validate the cell entry by user
+
     input_length = len(position)
     
     if input_length < 2 or input_length > 3:
@@ -130,6 +149,8 @@ def validate_cordinate(position: str) -> bool:
     return True
 
 def position_translator(position: str) -> int:
+    # Translate the position that user entered to integer between 1 to 100
+
     input_length = len(position)
     position = position.upper()
     char = position[input_length - 1]
@@ -163,6 +184,8 @@ def position_translator(position: str) -> int:
     return x + y
 
 def hit(position: int):
+    # Check the entry by the user that is duplicate or not, and if not duplicate check if it hits the ship or not
+
     global user_score
     cell = user_board[position]
     if cell.is_hit == True:
